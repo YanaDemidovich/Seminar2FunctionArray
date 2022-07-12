9a7db709768ecf4a290e3f26bfaf8c8616fbb842
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-13 00:16:24 +0300

    Почему то опять что-то надо было сохранить

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,15 +1,15 @@
-﻿int SumOfNums(int n1, int n2)
+﻿void SumOfNums(int n1, int n2)
 {
     int sum = n1 + n2;
-    return sum;
+    Console.WriteLine("Sum of elements is " + sum);
 }
 
 int number1 = 5;
 int number2 = 8;
-int result;
 
-result = SumOfNums(number1, number2);
-Console.WriteLine(result);
+SumOfNums(number1, number2);
+
+
 
 
 
