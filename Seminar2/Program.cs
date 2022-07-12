7bec81ac2f87d63523bf11ff23d8b53e3a895508
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-12 23:44:58 +0300

    Save1

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int SumOfNums(int n1, int n2)
+{
+    int sum = n1 + n2;
+    return sum;
+}
+
+int number1 = 5;
+int number2 = 8;
+int result;
+
+result = SumOfNums(number1, number2);
+Console.WriteLine(result);
+
+
+
+
