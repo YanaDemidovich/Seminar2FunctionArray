2459796ef9e1f6a51558d748e805aa3520e868aa
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-13 00:17:08 +0300

    Сохранила задачу №1

--- Seminar2Task1/Program.cs
+++ Seminar2Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача1. Написать программу, которая выводит случайное число из отрезка от 10 до 99 и показывает наибольшую цифру числа.
+
+int FineBiggerDigit()
+{
+    int rand = new Random().Next(10, 100);
+    Console.WriteLine("Current random number is " + rund);
+
+    int dec = rand / 10;
+    int ed = rand % 10;
+
+    if (dec > ed) return dec;
+    else return ed;
+}
+
+int maxNum = FineBiggerDigit();
+Console.WriteLine("Bigger digit is " + maxNum);
+
+
