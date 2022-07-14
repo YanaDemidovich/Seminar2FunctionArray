1a881cd39819503a80d3ab71dbeb99889c4ce10a
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-15 01:09:26 +0300

    Save Homework Task 13

--- Seminar2Homework/Program.cs
+++ Seminar2Homework/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Console.Write("Input number: ");
+/*Console.Write("Input number: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
 
 int Extract(int n1)
@@ -12,4 +12,26 @@ return number;
 }
 
 int num = Extract(n1);
-Console.WriteLine(num + " вторая цифра числа " + n1);
+Console.WriteLine(num + " вторая цифра числа " + n1);*/
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Input number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+void ExtractFalse(int n1)
+{
+    if (n1 < 99)
+    {
+    Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+    int ed = n1 / 10;
+    int dec = n1 / 100;
+    int num = ed - dec * 10;
+    Console.WriteLine("Третья цифра " + num);
+    }
+}
+
+ExtractFalse(n1);
