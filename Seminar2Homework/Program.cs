7205076aba0ebeb8759ffbcb49a5ed1f438e7298
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-14 23:50:50 +0300

    Save Homework Task 10

--- Seminar2Homework/Program.cs
+++ Seminar2Homework/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Input number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+int Extract(int n1)
+{
+int sot = n1 / 100; 
+int dec = n1 / 10;
+int number = dec - (sot * 10);
+return number;
+}
+
+int num = Extract(n1);
+Console.WriteLine(num + " вторая цифра числа " + n1);
